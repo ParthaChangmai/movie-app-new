608102a9ceefe596ca93f2753d01134560a4a3cf
Partha Changmai <partha.partha.changmai@gmail.com>
2022-03-05 16:23:57 +0530

    new

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./App.css";
 import Header from "./components/Header/Header";
 import SimpleBottomNavigation from "./components/MainNav";
 import Movies from "./Pages/Movies/Movies";
-import Search from "./Pages/Search/Search";
+
 import Series from "./Pages/Series/Series";
 import Trending from "./Pages/Trending/Trending";
 
@@ -18,7 +18,7 @@ function App() {
 						<Route path="/" component={Trending} exact />
 						<Route path="/movies" component={Movies} />
 						<Route path="/series" component={Series} />
-						<Route path="/search" component={Search} />
+						<Route path="/search" component={Series} />
 					</Switch>
 				</Container>
 			</div>

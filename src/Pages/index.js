13c8f7573aa5cb2684a568645d86e8866a8b0c5f
ParthaChangmai <partha.partha.changmai@gmail.com>
2022-03-25 05:22:51 +0530

    cleaner import for pages

---
+++ src/Pages/index.js
@@ -0,0 +1,4 @@
+export { default as Movies } from './Movies/Movies';
+export { default as Search } from './Search/Search';
+export { default as Series } from './Series/Series';
+export { default as Trending } from './Trending/Trending';

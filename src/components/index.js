c42e7c1aa51b389a66b1c5621bfffcecd0408ee7
ParthaChangmai <partha.partha.changmai@gmail.com>
2022-03-25 05:38:57 +0530

    for cleaner import of components

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+export { default as SimpleBottomNavigation } from './MainNav';
+export { default as Genres } from './Genres';
+export { default as SingContent } from './SingleContent/SingContent';
+export { default as CustomPagination } from './Pagination/CustomPagination';
+export { default as Header } from './Header/Header';
+export { default as ContentModal } from './contentModal/ContentModal';
+export { default as Carousel } from './Carousel/Carousel';
